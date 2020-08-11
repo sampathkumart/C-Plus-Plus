2be42332700b9d4d1783001f509aaa389567c945
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2020-08-11 10:09:00 +0530

    variables and global variables

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+   int num1;
+   double num2;
+
+//   std::cout << "enter a num:";
+//   std::cin >> num1;
+//   std::cout << "you entered: "<< num1 << std::endl;
+    
+   
+   std::cout << "integer:";
+   std::cin >> num1;
+   
+   std::cout << "double:";
+   std::cin >> num2;
+   
+   std::cout <<"integer is:" << num1 << std::endl;
+   std::cout <<"double is :" << num2 << std::endl;
+   
+}
---
+++ GlobalVariable/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int runs = 450;
+int main()
+{       
+    cout << "india scored " << runs << " runs" <<endl;
+    return 0;
+    
+}
