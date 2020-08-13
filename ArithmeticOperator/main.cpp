06ad3622cddd0d11dc5bd5102ace8548e0eb5514
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2020-08-13 14:26:36 +0530

    arithmetic operators

---
+++ ArithmeticOperator/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num1 = 180;
+int num2 = 90;
+
+cout << num1 << "+" << num2 << "=" <<num1 + num2 << endl;
+
+int result = 0;
+
+result =  num1 - num2;
+cout << num1 << "-" <<num2 << "=" << result << endl;
+
+result = num1 / num2;
+cout << num1 << "/" << num2 << "=" << result << endl;
+ 
+
+cout << 100 / 50 << endl;
+ 
+return 0;
+}
