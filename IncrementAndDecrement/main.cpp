81ef912ec5f871c3abc07708da1dc4c484d907e1
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2020-08-13 17:31:39 +0530

    increment and decrement operators

---
+++ IncrementAndDecrement/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+// simple Increment
+//{
+// int ball = 110;
+// 
+// cout << "Counter:" << ball << endl;
+// 
+// ball = ball +1;
+// cout << "Counter:" << ball << endl;
+// 
+// ball++ ;
+// cout << "Counter:" << ball << endl;
+//	
+//    
+// ++ball;
+//cout << "Counter:" << ball << endl;   
+//return 0;
+//}
+
+//preIncrement
+
+//{
+//    int fours = 50;
+//    int target = 0;
+//    
+//    cout  << "Fours:" << fours << endl;
+//    
+//    target = ++fours;
+//    cout << "Fours:" << fours << endl;
+//    cout << "target: " << target << endl;
+//    
+//    
+//    
+//    
+//    }
+
+{
+//    postIncrement
+//    
+//    int goal  = 8;
+//    int benalty = 3;
+//     
+//    cout << "Goal:" << goal << endl;
+//    
+//    benalty = goal++;
+//    cout << "goal:" << goal << endl;
+//    cout << "benalty:" << benalty << endl;
+//    
+    //
+    
+    int start = 1;
+    int finish = 2;
+    
+    cout << "start:" << start << endl;
+    
+    finish = ++ start +5;
+    cout << "statrt: " << start << endl;
+    cout << "end:" << finish << endl;
+    
+}
