3b3daaed42e2f609ac8305ec0ea1a0c2033045f4
Sathishkumar Rakkiasamy <rsathishtechit@gmail.com>
2020-08-13 12:44:52 +0530

    assignment operator

---
+++ AssignmentOperator/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int balls = 15;
+    int scores = 25;
+     int sixers = 90;
+    
+    balls = scores = sixers = 20;
+    
+    
+    cout << balls << " balls left" << endl;
+    cout << scores << " runs needed" << endl;
+    cout << sixers << " sixers hitted" << endl;
+    
+    return 0;
+    
+}
